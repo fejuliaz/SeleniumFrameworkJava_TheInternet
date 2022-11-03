ada3c151297edb8e319f368735270a7658f5e889
a263871 <fejuliaz@gmail.com>
2022-11-03 16:58:29 +0100

    Cookies, ChromeDriver Options

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -3,32 +3,36 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.CookieManager;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
 
 
-
 public class BaseTests {
 
-    private WebDriver driver;
+    private EventFiringWebDriver driver; //Listen to events
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
+        driver.register(new EventReporter());
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
+        //setCookies();
     }
 
     @BeforeMethod
@@ -45,16 +49,37 @@ public class BaseTests {
     public WindowManager getWindowManager() {
         return new WindowManager(driver);
     }
+    public CookieManager getCookieManager(){
+        return new CookieManager(driver);
+    }
+
+
+    //Options for the ChromeDriver
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        //options.setHeadless(true); //Run without starting ChromeDriver- faster tests results
+
+        return options;
+    }
+
+    /*
+        private void setCookies(){
+            Cookie cookie = new Cookie.Builder("tau","123")
+                    .domain("the-internet.herokuapp.com")
+                    .build();
+            driver.manage().addCookie(cookie);
+        }
+    */
 
     //Make screenshot when test fails
-
     @AfterMethod
     public void recordFailure(ITestResult result) {
-        if(ITestResult.FAILURE == result.getStatus()){
+        if (ITestResult.FAILURE == result.getStatus()) {
             var camera = (TakesScreenshot) driver;
             File screenshot = camera.getScreenshotAs(OutputType.FILE);
             try {
-                Files.move(screenshot, new File("resources/screenshots/" + result.getName()+ ".png"));
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
             } catch (IOException e) {
                 e.printStackTrace();
             }
